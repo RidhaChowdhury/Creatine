652e835914811572028e02bb090f572c81580230
Sam Lau <samalau@gmail.com>
2025-03-29 16:52:27 -0400

    nav bar styling

--- expo-app/app/(tabs)/_layout.tsx
+++ expo-app/app/(tabs)/_layout.tsx
@@ -1,20 +1,51 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
-import {CalendarFold} from 'lucide-react-native';
+import {CalendarFold, ChartNoAxesColumnIncreasing, Settings} from 'lucide-react-native';
+import { View } from 'react-native';
 
 const TabLayout = () => {
   return (
     <Tabs
     screenOptions={{
       headerShown: false, 
-      tabBarShowLabel: false 
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        backgroundColor: '#07070A',
+        borderTopColor: '#555A5F',
+        height: 75
+      },
+      tabBarItemStyle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8
+      }
     }}>
         <Tabs.Screen
             name="index"
             options={{
               headerShown: false,
               tabBarIcon: ({ focused }) => (
-                <CalendarFold size={24} color={focused ? 'accent' : 'gray'} />
+                <CalendarFold size={26} color={focused ? 'white' : '#555A5F' } />
+              )
+            }}
+        />
+
+        <Tabs.Screen
+            name="metrics"
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <ChartNoAxesColumnIncreasing size={26} color={focused ? 'white' : '#555A5F' } />
+              )
+            }}
+        />
+
+        <Tabs.Screen
+            name="settings"
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <Settings size={26} color={focused ? 'white' : '#555A5F' } />
               )
             }}
         />
