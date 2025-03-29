9cbee2aceb4e7f4a53a77121e69394e40b427afd
Ridha Chowdhury <chowdhuryridha@gmail.com>
2025-03-29 15:49:39 -0400

    quick fixes

--- expo-app/lib/supabase.js
+++ expo-app/lib/supabase.js
@@ -1,7 +1,9 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://xmkdzdqouxqmayoawztr.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhta2R6ZHFvdXhxbWF5b2F3enRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNzE2MDksImV4cCI6MjA1ODg0NzYwOX0.wtoAgSlQGSsnjdRwa7d6JJhrcjreFCSV9Jp2vbjDUM4";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhta2R6ZHFvdXhxbWF5b2F3enRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNzE2MDksImV4cCI6MjA1ODg0NzYwOX0.wtoAgSlQGSsnjdRwa7d6JJhrcjreFCSV9Jp2vbjDUM4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
