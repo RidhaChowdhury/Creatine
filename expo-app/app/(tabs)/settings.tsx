dc6be6a5788d8fc18a1d9ecc8e79dcb3db5231bd
Sam Lau <samalau@gmail.com>
2025-03-29 21:31:26 -0400

    Very good settings w/ database integration

--- expo-app/app/(tabs)/settings.tsx
+++ expo-app/app/(tabs)/settings.tsx
@@ -15,11 +15,39 @@ import { ChevronDownIcon } from '@/components/ui/icon'
 const Settings = () => {
     const { user } = useAuth()
     const [settings, setSettings] = React.useState<UserSettings | null>(null);
-    const [editProfile, setEditProfile] = React.useState(false);
-    const [editMetrics, setEditMetrics] = React.useState(false);
-    const [editGoals, setEditGoals] = React.useState(false);
-    const [editPreferences, setEditPreferences] = React.useState(false);
+    const [formState, setFormState] = React.useState({})
+    const [activeEditSection, setActiveEditSection] = React.useState<string | null>(null);
 
+    const toggleEdit = (section: string) => {
+        setActiveEditSection(prev => (prev === section ? null : section));
+    };
+    
+    const handleInputChange = (field: string, value: string) => {
+        setFormState(prev => ({
+            ...prev,
+            [field]: value
+        }));
+    };
+
+    const handleSave = async () => {
+        // console.log('Saving settings:', formState)
+        if (!settings) return;
+        const { error } = await supabase
+            .from('user_settings')
+            .update({
+                ...formState
+            })
+            .eq('user_id', user?.id)
+            .single()
+
+        if (error) {
+            console.error('Error updating user settings:', error)
+            return
+        }
+
+        setSettings({ ...settings, ...formState })
+        toggleEdit(activeEditSection!)
+    };
 
     React.useEffect(() => {
         const fetchSettings = async () => {
@@ -35,6 +63,7 @@ const Settings = () => {
             }
 
             setSettings(data)
+            setFormState(data)
         }
 
         fetchSettings()
@@ -50,16 +79,16 @@ const Settings = () => {
             <View>
                 <View className='flex-row items-center justify-between'>
                     <Text className="text-[20px] pt-[10] font-semibold pr-[5]">Profile Settings</Text>
-                    {(!editProfile) ? (
-                    <TouchableOpacity className='pt-[10]' onPress={() => setEditProfile(prev => !prev)}>
+                    {(activeEditSection !== 'profile') ? (
+                    <TouchableOpacity className='pt-[10]' onPress={() => toggleEdit('profile')}>
                         <Pencil color={'white'} size={18}></Pencil>
                     </TouchableOpacity>
                     ) : (
                         <View className='flex-row'>
-                            <TouchableOpacity className='pt-[10] pr-[25]' onPress={() => setEditProfile(prev => !prev)}>
+                            <TouchableOpacity className='pt-[10] pr-[25]' onPress={() => toggleEdit('profile')}>
                                 <X color={'white'} size={24}></X>
                             </TouchableOpacity>
-                            <TouchableOpacity className='pt-[10]' onPress={() => setEditProfile(prev => !prev)}>
+                            <TouchableOpacity className='pt-[10]' onPress={() => handleSave()}>
                                 <Check color={'white'} size={24}></Check>
                             </TouchableOpacity>
                         </View>
@@ -67,11 +96,12 @@ const Settings = () => {
                 </View>
 
                 <View>
-                    <Text className='text-[14px] py-[4]'>Name</Text>
-                    <Input isDisabled={!editProfile}>
-                        <InputField >{settings?.name}</InputField>
+                    <Text className='text-[14px] py-[4] text-typography-300'>Name</Text>
+                    <Input isDisabled={activeEditSection !== 'profile'} 
+                    >
+                        <InputField onChangeText={(e: string) => handleInputChange('name', e)} >{settings?.name}</InputField>
                     </Input>
-                    <Text className='text-[14px] py-[4]'>Email</Text>
+                    <Text className='text-[14px] py-[4] text-typography-300'>Email</Text>
                     <Input isDisabled={true} >
                         <InputField >{user?.email}</InputField>
                     </Input>
@@ -81,29 +111,29 @@ const Settings = () => {
             <View>
                 <View className='flex-row items-center justify-between'>
                     <Text className="text-[20px] pt-[15] font-semibold">Goals</Text>
-                    {(!editGoals) ? (
-                    <TouchableOpacity className='pt-[15]' onPress={() => setEditGoals(prev => !prev)}>
+                    {(activeEditSection !== 'goals') ? (
+                    <TouchableOpacity className='pt-[15]' onPress={() => toggleEdit('goals')}>
                         <Pencil color={'white'} size={18}></Pencil>
                     </TouchableOpacity>
                     ) : (
                         <View className='flex-row'>
-                            <TouchableOpacity className='pt-[15] pr-[25]' onPress={() => setEditGoals(prev => !prev)}>
+                            <TouchableOpacity className='pt-[15] pr-[25]' onPress={() => toggleEdit('goals')}>
                                 <X color={'white'} size={24}></X>
                             </TouchableOpacity>
-                            <TouchableOpacity className='pt-[15]' onPress={() => setEditGoals(prev => !prev)}>
+                            <TouchableOpacity className='pt-[15]' onPress={() => handleSave()}>
                                 <Check color={'white'} size={24}></Check>
                             </TouchableOpacity>
                         </View>
                     )}
                 </View>
                     <View>
-                        <Text className='text-[14px] py-[4]'>Water Goal ({settings?.water_unit})</Text>
-                        <Input isDisabled={!editGoals}>
-                            <InputField >{settings?.water_goal}</InputField>
+                        <Text className='text-[14px] py-[4] text-typography-300'>Water Goal ({settings?.water_unit})</Text>
+                        <Input isDisabled={activeEditSection !== 'goals'}>
+                            <InputField onChangeText={(e: string) => handleInputChange('water_goal', e)}>{settings?.water_goal}</InputField>
                         </Input>
-                        <Text className='text-[14px] py-[4]'>Creatine Goal ({settings?.supplement_unit})</Text>
-                        <Input isDisabled={!editGoals} >
-                            <InputField >{settings?.creatine_goal}</InputField>
+                        <Text className='text-[14px] py-[4] text-typography-300'>Creatine Goal ({settings?.supplement_unit})</Text>
+                        <Input isDisabled={activeEditSection !== 'goals'} >
+                            <InputField onChangeText={(e: string) => handleInputChange('creatine_goal', e)}>{settings?.creatine_goal}</InputField>
                         </Input>
                     </View>
             </View>
@@ -111,32 +141,32 @@ const Settings = () => {
             <View>
                 <View className='flex-row items-center justify-between'>
                     <Text className="text-[20px] pt-[15] font-semibold">Metrics</Text>
-                    {(!editMetrics) ? (
-                    <TouchableOpacity className='pt-[15]' onPress={() => setEditMetrics(prev => !prev)}>
+                    {(activeEditSection !== 'metrics') ? (
+                    <TouchableOpacity className='pt-[15]' onPress={() => toggleEdit('metrics')}>
                         <Pencil color={'white'} size={18}></Pencil>
                     </TouchableOpacity>
                     ) : (
                         <View className='flex-row'>
-                            <TouchableOpacity className='pt-[15] pr-[25]' onPress={() => setEditMetrics(prev => !prev)}>
+                            <TouchableOpacity className='pt-[15] pr-[25]' onPress={() => toggleEdit('metrics')}>
                                 <X color={'white'} size={24}></X>
                             </TouchableOpacity>
-                            <TouchableOpacity className='pt-[15]' onPress={() => setEditMetrics(prev => !prev)}>
+                            <TouchableOpacity className='pt-[15]' onPress={() => handleSave()}>
                                 <Check color={'white'} size={24}></Check>
                             </TouchableOpacity>
                         </View>
                     )}
                 </View>
                     <View>
-                        <Text className='text-[14px] py-[4]'>Height</Text>
-                        <Input isDisabled={!editMetrics}>
-                            <InputField >{settings?.height}</InputField>
+                        <Text className='text-[14px] py-[4] text-typography-300'>Height</Text>
+                        <Input isDisabled={activeEditSection !== 'metrics'}>
+                            <InputField onChangeText={(e: string) => handleInputChange('height', e)}>{settings?.height}</InputField>
                         </Input>
-                        <Text className='text-[14px] py-[4]'>Weight</Text>
-                        <Input isDisabled={!editMetrics} >
-                            <InputField >{settings?.weight}</InputField>
+                        <Text className='text-[14px] py-[4] text-typography-300'>Weight</Text>
+                        <Input isDisabled={activeEditSection !== 'metrics'} >
+                            <InputField onChangeText={(e: string) => handleInputChange('weight', e)}>{settings?.weight}</InputField>
                         </Input>
-                        <Text className='text-[14px] py-[4]'>Sex</Text>
-                        <Select isDisabled={!editMetrics}>
+                        <Text className='text-[14px] py-[4] text-typography-300'>Sex</Text>
+                        <Select isDisabled={activeEditSection !== 'metrics'} onValueChange={(value: string) => handleInputChange('sex', value)}>
                             <SelectTrigger variant="outline" size="md">
                                 <SelectInput placeholder={settings?.sex != null ? settings?.sex : 'Select'} />
                                 <SelectIcon className="mr-3" as={ChevronDownIcon} />
@@ -158,24 +188,24 @@ const Settings = () => {
             <View>
                 <View className='flex-row items-center justify-between'>
                     <Text className="text-[20px] pt-[15] font-semibold">Preferences</Text>
-                    {(!editPreferences) ? (
-                    <TouchableOpacity className='pt-[15]' onPress={() => setEditPreferences(prev => !prev)}>
+                    {(activeEditSection !== 'preferences') ? (
+                    <TouchableOpacity className='pt-[15]' onPress={() => toggleEdit('preferences')}>
                         <Pencil color={'white'} size={18}></Pencil>
                     </TouchableOpacity>
                     ) : (
                         <View className='flex-row'>
-                            <TouchableOpacity className='pt-[15] pr-[25]' onPress={() => setEditPreferences(prev => !prev)}>
+                            <TouchableOpacity className='pt-[15] pr-[25]' onPress={() => toggleEdit('preferences')}>
                                 <X color={'white'} size={24}></X>
                             </TouchableOpacity>
-                            <TouchableOpacity className='pt-[15]' onPress={() => setEditPreferences(prev => !prev)}>
+                            <TouchableOpacity className='pt-[15]' onPress={() => handleSave()}>
                                 <Check color={'white'} size={24}></Check>
                             </TouchableOpacity>
                         </View>
                     )}
                 </View>
                     <View>
-                        <Text className='text-[14px] py-[4]'>Water Unit</Text>
-                        <Select isDisabled={!editPreferences}>
+                        <Text className='text-[14px] py-[4] text-typography-300'>Water Unit</Text>
+                        <Select isDisabled={activeEditSection !== 'preferences'} onValueChange={(value: string) => handleInputChange('water_unit', value)}>
                             <SelectTrigger variant="outline" size="md">
                                 <SelectInput placeholder={settings?.water_unit} />
                                 <SelectIcon className="mr-3" as={ChevronDownIcon} />
@@ -191,8 +221,8 @@ const Settings = () => {
                                 </SelectContent>
                             </SelectPortal>
                         </Select>
-                        <Text className='text-[14px] py-[4]'>Supplement Unit</Text>
-                        <Select isDisabled={!editPreferences}>
+                        <Text className='text-[14px] py-[4] text-typography-300'>Supplement Unit</Text>
+                        <Select isDisabled={activeEditSection !== 'preferences'} onValueChange={(value: string) => handleInputChange('supplement_unit', value)}>
                             <SelectTrigger variant="outline" size="md">
                                 <SelectInput placeholder={settings?.supplement_unit} />
                                 <SelectIcon className="mr-3" as={ChevronDownIcon} />
@@ -213,7 +243,7 @@ const Settings = () => {
 
             
 
-            <Button className='mt-[15]' onPress={() => supabase.auth.signOut()}>
+            <Button className='mt-[15] mb-[15]' onPress={() => supabase.auth.signOut()}>
                 <ButtonText>Log Out</ButtonText>
             </Button>
         </ScrollView>
