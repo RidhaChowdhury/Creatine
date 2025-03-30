8fc343a8236e8a7badaf2993bf83a904e9bdd312
Ridha Chowdhury <chowdhuryridha@gmail.com>
2025-03-30 01:54:37 -0400

    Victory native installed chart kit removed

--- expo-app/app/(tabs)/metrics.tsx
+++ expo-app/app/(tabs)/metrics.tsx
@@ -1,21 +1,24 @@
-import { View, SafeAreaView, ScrollView } from 'react-native'
+import { View, SafeAreaView, ScrollView, useWindowDimensions } from 'react-native'
 import React from 'react'
 import { Text } from '@/components/ui/text'
 import { Box } from '@/components/ui/box'
 import { CalendarDays } from 'lucide-react-native'
-
+import { VStack } from '@/components/ui/vstack'
 
 const Metrics = () => {
+  const { width, height } = useWindowDimensions();
+  
   return (
     <SafeAreaView className="bg-background-0 h-full">
       <ScrollView className="px-[15]">
-        <View className="flex-row items-center pt-[10]">
-          <CalendarDays color={"white"} size={32} />
-          <Text className="text-[20px] font-semibold pl-[7]">History</Text>
+        <VStack>
+          <View className="flex-row items-center pt-[10]">
+            <CalendarDays color={"white"} size={32} />
+            <Text className="text-[20px] font-semibold pl-[7]">History</Text>
+          </View>
           <Box>
-
           </Box>
-        </View>
+        </VStack>
       </ScrollView>
     </SafeAreaView>
   );
