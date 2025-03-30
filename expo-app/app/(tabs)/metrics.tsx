146f8158a87b463abc3445d386322b5fd4e1fbb1
Ridha Chowdhury <chowdhuryridha@gmail.com>
2025-03-30 00:22:16 -0400

    chart package installed

--- expo-app/app/(tabs)/metrics.tsx
+++ expo-app/app/(tabs)/metrics.tsx
@@ -1,12 +1,24 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import { Text } from '@/components/ui/text'
+import { Box } from '@/components/ui/box'
+import { CalendarDays } from 'lucide-react-native'
+
 
 const Metrics = () => {
   return (
-    <SafeAreaView>
-      <Text>Metrics</Text>
+    <SafeAreaView className="bg-background-0 h-full">
+      <ScrollView className="px-[15]">
+        <View className="flex-row items-center pt-[10]">
+          <CalendarDays color={"white"} size={32} />
+          <Text className="text-[20px] font-semibold pl-[7]">History</Text>
+          <Box>
+
+          </Box>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 export default Metrics
