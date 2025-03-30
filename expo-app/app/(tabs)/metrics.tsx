80db1c0b7915b41747cd841fc91a7c65d6f536b6
Sam Lau <samalau@gmail.com>
2025-03-30 03:23:28 -0400

    bar chart that don't work

--- expo-app/app/(tabs)/metrics.tsx
+++ expo-app/app/(tabs)/metrics.tsx
@@ -4,13 +4,26 @@ import { Text } from '@/components/ui/text'
 import { Box } from '@/components/ui/box'
 import { CalendarDays } from 'lucide-react-native'
 import { VStack } from '@/components/ui/vstack'
+import { CartesianChart, Bar } from "victory-native";
+
 
 const Metrics = () => {
   const { width, height } = useWindowDimensions();
+  const fontSize = 24;
+
+  const dummyData = [
+    { x: 'Jan', y: 35 },
+    { x: 'Feb', y: 42 },
+    { x: 'Mar', y: 57 },
+    { x: 'Apr', y: 45 },
+    { x: 'May', y: 38 },
+    { x: 'Jun', y: 50 }
+  ];
+  
   
   return (
     <SafeAreaView className="bg-background-0 h-full">
-      <ScrollView className="px-[15]">
+      <ScrollView className="px-[25]">
         <VStack>
           <View className="flex-row items-center pt-[10]">
             <CalendarDays color={"white"} size={32} />
@@ -19,6 +32,28 @@ const Metrics = () => {
           <Box>
           </Box>
         </VStack>
+        <View>
+          <Text className="text-[32px] font-semibold pt-[10]">Metrics</Text>
+          <Box className='bg-primary-0 w-full h-[168] mt-[10] rounded-[15px]'>
+          <CartesianChart data={dummyData} xKey="x" yKeys={['y']}>
+      {({ points, chartBounds }) => (
+        <Bar
+          points={points.y}
+          chartBounds={chartBounds}
+          color="white"
+          roundedCorners={{ topLeft: 10, topRight: 10 }}
+          barWidth={15} // Adjust width if necessary
+          labels={{
+            position: "top",
+            color: "white",  // Customizing the label color only
+            font: null // Using default font
+
+          }}
+        />
+      )}
+    </CartesianChart>
+          </Box>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
