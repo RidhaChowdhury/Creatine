ff154323085859280d6e0eb7fb5f36f69d19718f
Ridha Chowdhury <chowdhuryridha@gmail.com>
2025-07-12 18:47:05 -0500

    test(ci): final test

--- expo-app/PrettierTest.tsx
+++ expo-app/PrettierTest.tsx
@@ -1,9 +1,6 @@
 // expo-app/PrettierTest.jsx
 export default function TestComponent() {
-	return (
-		<div>
-			<p>Unformatted content</p>
-			<p>test</p>
+	return (<div><p>Unformatted content</p><p>test</p>
 		</div>
 	);
 }
